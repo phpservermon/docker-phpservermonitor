e98c4d2bf661a0fc89092d5d7a594c27ba17e16e
TimZ99 <TimZ99@users.noreply.github.com>
2021-04-05 17:51:47 +0200

    Replaced links for network

--- maria-wait.sh
+++ maria-wait.sh
@@ -3,7 +3,7 @@
 set -ueo pipefail
 
 echo -n "Waiting until MariaDB has started"
-until (timeout 1 bash -c "< /dev/tcp/database/3306") &> /dev/null; do
+until (timeout 1 bash -c "< /dev/tcp/db/3306") &> /dev/null; do
 	echo -n .
 	sleep 1
 done
